0fc626cf7e60767450d42d0c6facc1b997e82a3f
diviner <qaz60908@gmail.com>
2021-01-27 15:54:46 +0800

    Add c04 practice

---
+++ c04/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func log(message string) {
+	fmt.Printf("This is log message %d", message)
+}
+
+func add(a, b int) int {
+	return a + b
+}
+
+func power(name string) (int, bool) {
+	if name == "devops" {
+		return 1, false
+	} else {
+		return 0, true
+	}
+}
+
+func main() {
+	value, exists := power("goku")
+	if exists == false {
+		fmt.Printf("value is %d , exists is %t", value, exists)
+	} else {
+		fmt.Printf("value is %d , exists is %t \n", value, exists)
+	}
+	// 如果只想要取得其中一個返回值
+	_, exists2 := power("devops")
+	if exists2 == false {
+		fmt.Printf("Error!! \n")
+	}
+}
