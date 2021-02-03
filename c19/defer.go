ed236083b35a6992138d32126c2064ee398943ba
diviner <qaz60908@gmail.com>
2021-02-03 16:08:16 +0800

    修正 c19/defer.go 的說明

--- c19/defer.go
+++ c19/defer.go
@@ -10,11 +10,24 @@ func demoDefer() {
 	fmt.Println("line2")
 }
 
+func demoNoneDefer() {
+	fmt.Println("line1")
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+	fmt.Println("line2")
+}
+
 func main() {
+	// 沒有 defer 的情況
+	fmt.Println("===== 沒 defer 的情況 =====")
+	demoNoneDefer()
+
 	// 驗證一下 defer 在何時執行
+	fmt.Println("===== 以下是有 defer 的情況 =====")
 	demoDefer()
 
-	// 理論上沒有 defer 的情況下 , 會先看到 line1 然後按序印出 4~0 最後在印出 line2
+	// 理論上沒有 defer 的情況下 , 會先看到 line1 然後按序印出 0~4 最後在印出 line2
 	// 但其實是會印出 line1 接著在印出 line2 , 最後才打印 4~0
 	// 而且是 4~0 並不是 0~4 , 這邊是有原因的 , 因為 defer 是 LIFO , 也就是說原本應該是要印 0~4 , 但 defer 的 LIFO 機制 , 讓 0 變成最先印的變成最後印
 
